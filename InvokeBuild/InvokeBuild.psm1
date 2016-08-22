6267f12eba5ea60cdf64e07a3fe0993231efd2fe
Roman Kuzmin <nightroman@gmail.com>
2016-08-22 18:55:09 +0100

    fix slashes #31

--- InvokeBuild/InvokeBuild.psm1
+++ InvokeBuild/InvokeBuild.psm1
@@ -1,4 +1,4 @@
 
-Set-Alias -Name Invoke-Build -Value $PSScriptRoot\Invoke-Build.ps1
-Set-Alias -Name Invoke-Builds -Value $PSScriptRoot\Invoke-Builds.ps1
+Set-Alias -Name Invoke-Build -Value (Join-Path $PSScriptRoot Invoke-Build.ps1)
+Set-Alias -Name Invoke-Builds -Value (Join-Path $PSScriptRoot Invoke-Builds.ps1)
 Export-ModuleMember -Alias Invoke-Build, Invoke-Builds
