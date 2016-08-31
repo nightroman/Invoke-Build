bada83e113167590d09e08192b95d27d10f8568c
Roman Kuzmin <nightroman@gmail.com>
2016-08-31 06:01:54 +0100

    Fix #33

--- Invoke-Build.ArgumentCompleters.ps1
+++ Invoke-Build.ArgumentCompleters.ps1
@@ -1,6 +1,6 @@
 
 <#PSScriptInfo
-.VERSION 1.0.0
+.VERSION 1.0.1
 .AUTHOR Roman Kuzmin
 .COPYRIGHT (c) Roman Kuzmin
 .GUID 78b68f80-80c5-4cc1-9ded-e2ae165a9cbd
@@ -35,7 +35,7 @@
 Register-ArgumentCompleter -CommandName Invoke-Build.ps1 -ParameterName Task -ScriptBlock {
 	param($commandName, $parameterName, $wordToComplete, $commandAst, $boundParameters)
 
-	(& $commandName ?? -File ($boundParameters['File'])).Keys -like "$wordToComplete*" | .{process{
+	(& 'Invoke-Build' ?? -File ($boundParameters['File'])).Keys -like "$wordToComplete*" | .{process{
 		New-Object System.Management.Automation.CompletionResult $_, $_, 'ParameterValue', $_
 	}}
 }
