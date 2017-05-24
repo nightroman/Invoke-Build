a02de2816d4352bd9f5e49eafd65922e101b9003
Roman Kuzmin <nightroman@gmail.com>
2017-05-24 10:49:47 +0100

    Rework Invoke-TaskFromVSCode.ps1, use $psEditor, Invoke-Build

--- Invoke-TaskFromVSCode.ps1
+++ Invoke-TaskFromVSCode.ps1
@@ -1,6 +1,6 @@
 
 <#PSScriptInfo
-.VERSION 1.0.3
+.VERSION 1.0.4
 .AUTHOR Roman Kuzmin
 .COPYRIGHT (c) Roman Kuzmin
 .TAGS Invoke, Task, Invoke-Build, VSCode
@@ -11,42 +11,36 @@
 
 <#
 .Synopsis
-	Invokes the current task from VSCode by Invoke-Build.ps1
+	Invokes the current Invoke-Build task from VSCode
 
 .Description
-	This script invokes the current task from the build script in VSCode. It is
-	invoked either in the VSCode session or in an external PowerShell console.
+	This script invokes the current task from the build script in VSCode.
+	It is invoked in the session or in an external PowerShell console.
 	The script requires the VSCode PowerShell extension.
 
-	The current task is the task at the caret line or above. If none is found
-	then the default task is invoked. Currently the script should be saved
-	manually before invoking.
+	The current task is the task at the caret or above. If none is found then
+	the default task is invoked. Currently the script should be saved manually.
 
-	In order to register editor commands create or open the VSCode profile:
+	Invoke the script directly from the integrated console or register it for
+	PowerShell.ShowAdditionalCommands. Create or open the VSCode profile:
 
 		C:\Users\...\Documents\WindowsPowerShell\Microsoft.VSCode_profile.ps1
 
-	and add two commands:
+	and add commands:
 
-		Register-EditorCommand -Name IB -DisplayName 'Invoke task' -ScriptBlock {
-			param($Context)
+		Register-EditorCommand -Name IB1 -DisplayName 'Invoke task' -ScriptBlock {
 			Invoke-TaskFromVSCode.ps1
 		}
 
-		Register-EditorCommand -Name IBConsole -DisplayName 'Invoke task in console' -SuppressOutput -ScriptBlock {
-			param($Context)
+		Register-EditorCommand -Name IB2 -DisplayName 'Invoke task in console' -SuppressOutput -ScriptBlock {
 			Invoke-TaskFromVSCode.ps1 -Console
 		}
 
-	These commands assume that Invoke-TaskFromVSCode.ps1 is in the path.
-	If this is not the case then specify the full script path.
-
-	In order to invoke commands, press F1 or Ctrl+Shift+P to open the command
-	palette. Type "addi" until you see the item "PowerShell: Show Additional
-	Commands", then press Enter.
+	Specify the full path if Invoke-TaskFromVSCode.ps1 is not in the path.
+	You can add a keyboard shortcut for PowerShell.ShowAdditionalCommands.
 
 .Parameter Console
-		Tells to invoke the current task in a new PowerShell console.
+		Tells to invoke the task in an external PowerShell console.
 #>
 
 param(
@@ -54,37 +48,36 @@ param(
 	[switch]$Console
 )
 
+trap {$PSCmdlet.ThrowTerminatingError($_)}
 $ErrorActionPreference = 'Stop'
 
-$private:ib = "$(Split-Path $MyInvocation.MyCommand.Path)\Invoke-Build.ps1"
-if (!(Test-Path -LiteralPath $ib)) {
-	$ib = 'Invoke-Build'
+$private:file = $null
+try {
+	$private:context = $psEditor.GetEditorContext()
+	$file = $context.CurrentFile
 }
+catch {}
+if (!$file) {throw 'Cannot get the current file.'}
 
 $private:_Console = $Console
 Remove-Variable Console
 
-$private:file = $Context.CurrentFile
-if (!$file) {Write-Error "There is not a current file."}
-
 $private:path = $file.Path
-if ($path -notlike '*.ps1') {Write-Error "The current file must be '*.ps1'."}
+if ($path -notlike '*.ps1') {throw "The current file must be '*.ps1'."}
 
 $private:task = '.'
-$private:line = $Context.CursorPosition.Line
-foreach($private:t in (& $ib ?? $path).Values) {
+$private:line = $context.CursorPosition.Line
+foreach($private:t in (Invoke-Build ?? $path).Values) {
 	if ($t.InvocationInfo.ScriptName -ne $path) {continue}
 	if ($t.InvocationInfo.ScriptLineNumber -gt $line) {break}
 	$task = $t.Name
 }
 
 if ($_Console) {
-	Start-Process PowerShell.exe ("-NoExit -NoProfile -ExecutionPolicy Bypass & '{0}' '{1}' '{2}'" -f @(
-		$ib.Replace("'", "''")
-		$task.Replace("'", "''").Replace('"', '\"')
-		$path.Replace("'", "''")
-	))
+	$command = "Invoke-Build '$($task.Replace("'", "''"))' '$($path.Replace("'", "''"))'"
+	$encoded = [Convert]::ToBase64String(([System.Text.Encoding]::Unicode.GetBytes($command)))
+	Start-Process powershell.exe "-NoExit -NoProfile -ExecutionPolicy Bypass -EncodedCommand $encoded"
 }
 else {
-	& $ib $task $path
+	Invoke-Build $task $path
 }
--- Tests/Invoke-TaskFrom/Invoke-TaskFromVSCode.test.ps1
+++ Tests/Invoke-TaskFrom/Invoke-TaskFromVSCode.test.ps1
@@ -4,64 +4,78 @@
 	Tests Invoke-TaskFromISE.ps1
 #>
 
-. .\Tools.ps1
+. ./Tools.ps1
 
-# Mock $Context
-function New-Context($Line) {
+# Fake $psEditor and $Context
+function Use-Context {
 	$file = New-Object psobject
-	$file | Add-Member -MemberType NoteProperty -Name Path -Value $TestFilePath
+	Add-Member -InputObject $file -MemberType NoteProperty -Name Path -Value $TestFilePath
 
 	$cursor = New-Object psobject
-	$cursor | Add-Member -MemberType NoteProperty -Name Line -Value $Line
+	Add-Member -InputObject $cursor -MemberType NoteProperty -Name Line -Value 0
 
-	$r = New-Object psobject
-	$r | Add-Member -MemberType NoteProperty -Name CurrentFile -Value $file
-	$r | Add-Member -MemberType NoteProperty -Name CursorPosition -Value $cursor
-	Set-Variable -Name Context -Value $r -Scope 1
+	$Context = New-Object psobject
+	Add-Member -InputObject $Context -MemberType NoteProperty -Name CurrentFile -Value $file
+	Add-Member -InputObject $Context -MemberType NoteProperty -Name CursorPosition -Value $cursor
+	Set-Variable -Name Context -Value $Context -Scope 1
+
+	$psEditor = New-Object psobject
+	Add-Member -InputObject $psEditor -MemberType ScriptMethod -Name GetEditorContext -Value {$Context}
+	Set-Variable -Name psEditor -Value $psEditor -Scope 1
 }
 
 task test-file-null {
-	New-Context
+	Use-Context
 	$Context.CurrentFile = $null
+
 	($r = try {Invoke-TaskFromVSCode.ps1} catch {$_})
-	equals "$r" 'There is not a current file.'
+	equals "$r" 'Cannot get the current file.'
 }
 
 task test-file-not-ps1 {
-	New-Context
+	Use-Context
 	$Context.CurrentFile.Path = 'test.txt'
+
 	($r = try {Invoke-TaskFromVSCode.ps1} catch {$_})
 	equals "$r" "The current file must be '*.ps1'."
 }
 
 task test-no-task {
-	New-Context (Find-Line test-no-task)
+	Use-Context
+	$Context.CursorPosition.Line = Find-Line test-no-task
 
 	($r = Invoke-TaskFromVSCode.ps1)
 	assert ($r -contains '//.//')
 }
 
 task test-t1-first-line {
-	New-Context (Find-Line test-t1-first-line)
+	Use-Context
+	$Context.CursorPosition.Line = Find-Line test-t1-first-line
+
 	($r = Invoke-TaskFromVSCode.ps1)
 	assert ($r -contains '//t1//')
 }
 
 task test-t1-inner-line {
-	New-Context (Find-Line test-t1-inner-line)
+	Use-Context
+	$Context.CursorPosition.Line = Find-Line test-t1-inner-line
+
 	($r = Invoke-TaskFromVSCode.ps1)
 	assert ($r -contains '//t1//')
 }
 
 task test-t1-after-line {
-	New-Context (Find-Line test-t1-after-line)
+	Use-Context
+	$Context.CursorPosition.Line = Find-Line test-t1-after-line
+
 	($r = Invoke-TaskFromVSCode.ps1)
 	assert ($r -contains '//t1//')
 }
 
 task test-fail {
-	$line = Find-Line test-fail
-	New-Context $line
+	Use-Context
+	$Context.CursorPosition.Line = Find-Line test-fail
+
 	$r = ''
 	try {Invoke-TaskFromVSCode.ps1} catch {$r = $_}
 	equals "$r" Oops!
