ed240a3565062d779bf5f689d60281f6ecdf5155
Roman Kuzmin <nightroman@gmail.com>
2018-07-20 05:40:12 +0100

    Fix Tests/ArgumentCompleters

--- Tests/ArgumentCompleters/.test.ps1
+++ Tests/ArgumentCompleters/.test.ps1
@@ -2,12 +2,12 @@
 $Version = $PSVersionTable.PSVersion.Major
 
 task TabExpansion2.v5 -If ($Version -ge 5) {
-	exec {ib.cmd * TabExpansion2.v5.build.ps1}
+	exec {Invoke-PowerShell -NoProfile -Command Invoke-Build * TabExpansion2.v5.build.ps1}
 }
 
 task TabExpansionPlusPlus -If ($Version -ge 3) {
 	if (Get-Module TabExpansionPlusPlus -ListAvailable) {
-		exec {ib.cmd * TabExpansionPlusPlus.build.ps1}
+		exec {Invoke-PowerShell -NoProfile -Command Invoke-Build * TabExpansionPlusPlus.build.ps1}
 	}
 	else {
 		Write-Warning 'Missing module TabExpansionPlusPlus'
