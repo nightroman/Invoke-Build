dc3a80a966c96d02ac68d5b76bff9a09bd047bfa
Roman Kuzmin <nightroman@gmail.com>
2019-09-06 06:54:32 +0100

    Add test PositionalParameters

--- Tests/Fixed.test.ps1
+++ Tests/Fixed.test.ps1
@@ -494,7 +494,7 @@ task PreserveCheckpoint {
 	Remove-Item z.build.ps1
 }
 
-# Issue #152. Also test the bootstrapping scenario.
+# Issue #152 (1). Also test the bootstrapping scenario.
 # This used to fail on the second call of z.ps1.
 task DoNotMakeScriptParametersNamed {
 	# self-invoking build script
@@ -514,4 +514,29 @@ task DoNotMakeScriptParametersNamed {
 	./z.ps1 Test
 
 	remove z.ps1
+}
+
+<#
+	Issue #152 (2). Test positional parameters.
+	Potential problem:
+		- `Task` and `P1` both have position 0.
+		- `File` and `P2` both have position 1.
+	Fortunate current behaviour:
+		PowerShell somehow does what we expect, shift positions of P1, P2, P3.
+		This may change in the future, so let at least cover this by test.
+	Workarounds for the future:
+		- Use `[CmdletBinding(PositionalBinding=$false)]` (PowerShell v3+) to enforce named parameters.
+		- Set parameter positions explicitly starting with 2 (0, 1 are consumed by Task, File).
+#>
+task PositionalParameters {
+	Set-Content z.build.ps1 {
+		param($P1, $P2, $P3)
+		task Parameters {"$P1|$P2|$P3"}
+	}
+
+	# invoke the script with 5 positional parameters
+	($r = Invoke-Build Parameters z.build.ps1 v1 v2 v3)
+	assert ($r -contains 'v1|v2|v3')
+
+	remove z.build.ps1
 }
