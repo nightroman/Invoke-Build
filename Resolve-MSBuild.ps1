b2e3834a57631fc077df4a7467734daaa9b26bc5
Manfred Wallner <mwallner@users.noreply.github.com>
2026-01-28 13:39:30 +0100

    GH-244: vs2026 is correctly resolved with current v18.2.1 (#245)
    Co-authored-by: Manfred Wallner <manfred.wallner@ksengineers.com>

--- Resolve-MSBuild.ps1
+++ Resolve-MSBuild.ps1
@@ -155,6 +155,8 @@ function Get-MSBuild15Guess {
 			"$Program86\Microsoft Visual Studio\Preview"
 		}
 		elseif ($Version -eq '*') {
+			"$Program64\Microsoft Visual Studio\2026"
+			"$Program86\Microsoft Visual Studio\2026"
 			"$Program64\Microsoft Visual Studio\18"
 			"$Program86\Microsoft Visual Studio\18"
 			"$Program64\Microsoft Visual Studio\2022"
@@ -163,6 +165,8 @@ function Get-MSBuild15Guess {
 			"$Program86\Microsoft Visual Studio\2017"
 		}
 		elseif ($Version -eq '18.0') {
+			"$Program64\Microsoft Visual Studio\2026"
+			"$Program86\Microsoft Visual Studio\2026"
 			"$Program64\Microsoft Visual Studio\18"
 			"$Program86\Microsoft Visual Studio\18"
 		}
